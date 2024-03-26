2c51d025a2d51ceb2fad4ef26eb64388ba58c26a
KerolisAymen <kerolisaymen456@gmail.com>
2024-03-26 16:34:57 +0200

    11

--- app.js
+++ app.js
@@ -5,32 +5,40 @@ const app = express();
 const bodyParser = require("body-parser");
 const qr = require("qrcode");
 const axios = require("axios");
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 // Set the view engine to use EJS
 app.set("view engine", "ejs");
 
 // Set the directory for EJS templates (optional if you're using a single folder)
-app.set("views", __dirname); // Use the current directory where server.js is located
+app.set("views", __dirname);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname)));
 app.use(express.json());
-
-const studentSchema = new mongoose.Schema({
+const meetingSchema = new mongoose.Schema({
   name: String,
-  grade: Number,
-  profilepic : String,
-  meetings: Array,
+  date: Date,
 });
 
-const courseSchema = new mongoose.Schema({
-  name: String,
+const studentSchema = new mongoose.Schema
+({
+    ID : String,
+    name: String,
+    grade: Number,
+    profilepic: String,
+    meetings: [
+      {
+        _id: false ,
+        meeting: { type: mongoose.Schema.Types.ObjectId, ref: "Meeting" },
+        bonusandminus: [Number], // Array of strings for bonus and minus data
+      },
+    ],
 });
 
 const Student = mongoose.model("Student", studentSchema);
-const Course = mongoose.model("Course", courseSchema);
+const Meeting = mongoose.model("Meeting", meetingSchema);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
@@ -39,118 +47,122 @@ app.get("/registration", (req, res) => {
   res.sendFile(path.join(__dirname, "registration.html"));
 });
 
-app.post("/registration.html", upload.single('avatar'), async(req, res ,next) => {
+app.post("/registration.html",upload.single("avatar"),
+  async (req, res, next) => {
+    const username = req.body.username;
+    // console.log(req.body);
 
-
-
-
-  const username = req.body.username;
-  // console.log(req.body);
-
-  const grade = req.body.grade;
-
-  let newStudent = new Student({
-    name: username,
-    profilepic: req.file.path,
-    grade: grade,
-  });
-
-  if(await Student.findOne({name :req.body.username})!=undefined)
-  {
-    console.log(await Student.findOne({name:req.body.username}));
-    res.send("<h1>تم التسجيل من قبل</h1>");
-  }else{
-  newStudent
-    .save()
-    .then((s) => {
-      console.log(s);
-      res.send("<h1>تم التسجيل بنجاح</h1>");
-    })
-    .catch((error) => {
-      console.log(error);
+    const grade = req.body.grade;
+    let newuserid = Math.floor((Math.random() * 99999) + 10000)
+    let newStudent = new Student({
+      ID : newuserid , 
+      name: username,
+      profilepic: req.file.path,
+      grade: grade,
     });
-  }
-});
 
-app.get("/attendanceReview/:name", async (req, res) => {
-  const profileData = await Student.findOne({ name: req.params.name });
+    if ((await Student.findOne({ name: req.body.username })) != undefined) {
+      console.log(await Student.findOne({ name: req.body.username }));
+      res.send("<h1>تم التسجيل من قبل</h1>");
+    } else {
+      newStudent
+        .save()
+        .then((s) => {
+          console.log(s);
+          res.send(`<h1>تم التسجيل بنجاح</h1> <h2>your id :</h2><h2> ${newuserid} </h2>`);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }
+);
+
+app.get("/attendanceReview/:ID", async (req, res) => {
+  const profileData = await Student.findOne({ ID: req.params.ID }).populate(
+    "meetings.meeting"
+  );
   // console.log(profileData.meetings.length)
-  if(profileData != undefined){
-  let qrsrc;
-  const apiUrl = "https://api.qrserver.com/v1/create-qr-code/";
-  const params = {
-    size: "300x300",
-    data: req.params.name,
-  };
+  if (profileData != undefined) {
+    let qrsrc;
+    const apiUrl = "https://api.qrserver.com/v1/create-qr-code/";
+    const params = {
+      size: "300x300",
+      data: req.params.ID,
+    };
 
-  await axios
-    .get(apiUrl, { params })
-    .then((response) => {
-      console.log("QR code URL:", response.request.res.responseUrl);
-      qrsrc = response.request.res.responseUrl;
-    })
-    .catch((error) => {
-      console.error("Error:", error.message);
-    });
+    await axios
+      .get(apiUrl, { params })
+      .then((response) => {
+        console.log("QR code URL:", response.request.res.responseUrl);
+        qrsrc = response.request.res.responseUrl;
+      })
+      .catch((error) => {
+        console.error("Error:", error.message);
+      });
 
-  const profileData2 = {
-    name: profileData.name,
-    grade: profileData.grade,
-    meetingsAttended: profileData.meetings.length,
-    profilepic : profileData.profilepic,
-    imgUrl: qrsrc,
-  };
+    console.log(profileData.meetings);
+    const profileData2 = {
+      name: profileData.name,
+      grade: profileData.grade,
+      meetingsAttended: profileData.meetings.length,
+      meetings: profileData.meetings,
+      profilepic: profileData.profilepic,
+      imgUrl: qrsrc,
+    };
 
-  JSON.stringify(profileData2);
-  console.log(profileData2);
-  res.render("profile", { profileData2 });
-}
-else{
-  res.send("<h1>this user not found</h1>")
-}
-});
-
-// if ("كرستين جمال كامل"=="كرستين جمال كامل")console.log("yes = ")
-app.post("/qrcodepage", async (req, res) => {
-  // console.log(req.body.decodeText);
-  // console.log(req.body.decodeText);
-
-  let name = req.body.decodeText;
-  const name2 = await Student.findOne({ name: name });
-
-  if (name2!= undefined)
-  {
-  console.log(name2);
-  console.log(name2.meetings);
-
-  let today = new Date(new Date().setHours(0, 0, 0, 0));
-  
-   if (name2.meetings.length == 0 ||name2.meetings[name2.meetings.length - 1].toString() != today.toString()){
-    name2.meetings.push(today);
-    await name2.save().then(() => {
-      res.json({ message: "saved successfully", success: true });
-      console.log("saved successfully");
-    });
-  }else{
-    res.json({ message: "saved before", success: true });
+    JSON.stringify(profileData2);
+    res.render("profile", { profileData2 });
+  } else {
+    res.send("<h1>this user not found</h1>");
   }
-}
-else res.json({ message: "this qr code is not found", success: false });
 });
 
-app.get("/dashboard" , async(req,res)=>{
-console.log(await Student.find({},"name"));
-const users = await Student.find({}) ; 
-res.render('dashboard', { users });
+app.post("/qrcodepage", async (req, res) => {
+  console.log(req.body);
+  const name = await Student.findOne({ ID: req.body.decodeText });
+  const meeting1 = await Meeting.findById(req.body.selectedValue);
+  const newdata = await {
+    meeting: meeting1.id,
+  };
+  const arr = await name.meetings.filter((object) => object.meeting.ObjectId === meeting1.id);
+  // console.log(name); 
+  // console.log(name.meetings[0].meeting.toString());
+  // console.log(meeting1.id);
+  // console.log(arr); 
+  if (await name.meetings.filter((object) => object.meeting.toString() === meeting1.id).length == 0) { // use this here
+    await name.meetings.push(newdata);
+    await name.save()
+      .then((saveddata) => {
+        res.send({ message:" saved", success: true });
+      })
+      .catch((err) => {
+        res.send({message:"error saving"})
+      });
+  } else {
+    res.send({ message:"saved before" });
+  }
 });
 
+app.get("/dashboard", async (req, res) => {
+  const users = await Student.find({}) //.populate("meetings.meeting");
+  console.log(users);
+  res.render("dashboard", { users });
+});
 
-
-
-
-
-
-
+app.get("/getmeetings", async (req, res) => {
+  res.send(await Meeting.find({}));
+});
+app.post("/putmeetings", async (req, res) => {
+  console.log(req.body);
+  const newmeeting = new Meeting({
+    name: req.body.type,
+    date: req.body.date,
+  });
+  newmeeting.save().then((data) => {
+    res.send("added successfully");
+  });
+});
 
 mongoose
   .connect(
--- script.js
+++ script.js
@@ -1,57 +1,83 @@
 // script.js file
 
 function domReady(fn) {
-	if (
-		document.readyState === "complete" ||
-		document.readyState === "interactive"
-	) {
-		setTimeout(fn, 1000);
-	} else {
-		document.addEventListener("DOMContentLoaded", fn);
-	}
+  if (
+    document.readyState === "complete" ||
+    document.readyState === "interactive"
+  ) {
+    setTimeout(fn, 1000);
+  } else {
+    document.addEventListener("DOMContentLoaded", fn);
+  }
 }
-const map1 = new Map();
+
 domReady(function () {
 
-	// If found you qr code
-	function onScanSuccess(decodeText, decodeResult) {
-		// alert("You Qr is : " + decodeText, decodeResult);
-        var beepSound = document.getElementById("beep");
-    beepSound.play();
-    console.log(decodeText);
-    // console.log(decodeResult); 
-	console.log(JSON.stringify({decodeText}));
-	map1.set(decodeText).val++; 
-	fetch( "/qrcodepage",{
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({decodeText})
-	})
-	.then(response => {
-		if (!response.ok) {
-		  throw new Error('Network response was not ok');
-		}
-		return response.json();
-	  })
-	  .then(data => {
-		
-		  alert(decodeText +" "+ data.message);
-		
-	  })
-	  .catch(error => {
-		console.error('There was a problem with the fetch operation:', error);
-	  });
+  const meetingselect = document.getElementById("meeting-select");
 
+  function onScanSuccess(decodeText, decodeResult) {
+
+
+
+
+
+    if (meetingselect.value != "") {
+		var beepSound = document.getElementById("beep");
+		beepSound.play();
+      fetch("/qrcodepage", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          decodeText,
+          selectedValue: meetingselect.value,
+        }),
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          alert(decodeText + " " + data.message);
+        })
+        .catch((error) => {
+          console.error("There was a problem with the fetch operation:", error);
+        });
+    }
+	else{
+		alert("choose meeting!!!");
 	}
+  }
 
-	let htmlscanner = new Html5QrcodeScanner(
-		"my-qr-reader",
-		{ fps: 3, qrbos: 250 }
-	);
-	htmlscanner.render(onScanSuccess);
+  let htmlscanner = new Html5QrcodeScanner("my-qr-reader", {
+    fps: 3,
+    qrbos: 250,
+  });
+  htmlscanner.render(onScanSuccess);
 });
 
+fetch("/getmeetings")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
 
-// Make a request to the backend endpoint
+    data.forEach((element) => {
+      var select = document.getElementById("meeting-select");
+      var option = document.createElement("option");
+      option.value = element._id;
+      option.text = element.name +"---"+element.date;
+      console.log(element._id);
+      select.appendChild(option);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
